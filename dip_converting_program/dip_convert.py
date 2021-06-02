5392062401b00356260f17fa464acecc2964d6d7
Zvezda Tsoi <62754976+tsoinikolas@users.noreply.github.com>
2021-06-03 01:31:11 +0900

    Update dip_convert.py

--- dip_converting_program/dip_convert.py
+++ dip_converting_program/dip_convert.py
@@ -1,6 +1,36 @@
-dips = int(input("insert your dip value : "))
-directions = int(input("insert your dip direction value : "))
+####this is a hard-coded dip direction convert program made with python.
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+#prompt some hello message
+print(color.BOLD + color.CYAN + """\n\n\n##############################################
+##                                          ##
+##             Dip-Directionist             ##
+##           wrote by jungwon choi          ##
+##                                          ##
+##############################################
+""" + color.END)
+
+print(color.BOLD +"Introduction"+color.END)
+print("\tyou aquired dip-dipdirection data from your field, right?")
+print("\tnow all you need to do is insert data to terminal!\n")
+
+
+
+
+dips = int(input(color.BOLD + "insert your dip value : " + color.GREEN ))
+directions = int(input(color.END + color.BOLD + "insert your dip direction value : " + color.GREEN))
+print(color.END)
 direc = (directions - 90)
 direc_after = (360-(directions + 90))
 
